7a9f0675512d7510e77e5f689ed05de3fb72abfa
xiaoz <xiaoz93@outlook.com>
2021-03-06 09:40:33 +0800

    fix bug

--- client/run.sh
+++ client/run.sh
@@ -2,6 +2,6 @@
 #/usr/bin/brook relay -f :9999 -t ${DIP}:$DPORT
 #/usr/bin/brook server -l :9999 -p ${PASSWORD}
 #start socat
-nohup /usr/bin/brook server client -s ${SERVER} -p ${PASSWORD} --socks5 0.0.0.0:1080 &
+nohup /usr/bin/brook client -s ${SERVER} -p ${PASSWORD} --socks5 0.0.0.0:1080 &
 #start http
 /usr/bin/brook socks5tohttp -s 127.0.0.1:1080 -l 0.0.0.0:8010
